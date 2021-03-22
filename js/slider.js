bb11f7c18cabf1c3011554ddaedb43199f248ec7
apgrayedd <56434676+apgrayedd@users.noreply.github.com>
2021-03-22 14:13:09 +0300

    доработка слайдера

--- js/slider.js
+++ js/slider.js
@@ -6,27 +6,36 @@ const button_3 = slider.querySelector('.slider-button-3');
 const slider_image = slider.querySelector('.slider_image');
 const slider_text = slider.querySelector('p');
 let buttons = [button_1,button_2,button_3];
+let slider_storage = localStorage.getItem('slider')
 
-let slider_chager = function(button,i){
+let slider_changer = function(button,i){
+  console.log(button);
+  let button_selecter = document.querySelector('.slider-radio-selected');
+  button_selecter.classList.remove("slider-radio-selected");
+  button.classList.add("slider-radio-selected");
+
+  body.className = "page-body slider-"+(Number(i)+1);
+  slider_image.src = "img/slider_"+(Number(i)+1)+".png";
+  localStorage.setItem('slider',i);
+
+  if(i === 0){
+      slider_text.textContent = "Пломбир с помадкой и клубничный щербет";
+  }
+  if(i == 1){
+      slider_text.textContent = "Шоколадный пломбир и лимонный сорбет";
+  }
+  if(i == 2){
+      slider_text.textContent = "Крем-брюле и пломбир с малиновым джемом";
+  }
+}
+let slider_changer_event = function(button,i){
     button.addEventListener('click',function(){
-        let button_selecter = document.querySelector('.slider-radio-selected');
-        button_selecter.classList.remove("slider-radio-selected");
-        button.classList.add("slider-radio-selected");
-
-        body.className = "page-body slider-"+(i+1);
-        slider_image.src = "img/slider_"+(i+1)+".png";
-
-        if(i === 0){
-            slider_text.textContent = "Пломбир с помадкой и клубничный щербет";
-        }
-        if(i == 1){
-            slider_text.textContent = "Шоколадный пломбир и лимонный сорбет";
-        }
-        if(i == 2){
-            slider_text.textContent = "Крем-брюле и пломбир с малиновым джемом";
-        }
+      slider_changer(button,i);
     });
 }
+if(slider_storage){
+  slider_changer(buttons[slider_storage],slider_storage);
+}
 for(let i = 0; i < buttons.length; i++){
-    slider_chager(buttons[i],i);
+  slider_changer_event(buttons[i],i);
 }
